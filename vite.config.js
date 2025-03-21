657b3517a9c7faa0fed1d54a4fab263e56a36b97
DJBOLLA295 <145054446+DJBOLLA295@users.noreply.github.com>
2025-03-21 22:26:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Usa il nome del tuo repository "React"
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/React/', 
+});
